70ad4cc27df87be4a808e6b401b37991465971b3
CatsaCode <catsacode@gmail.com>
2024-11-26 20:11:44 -0500

    Revert "Test custom types"
    This reverts all of the shenanigans about moving every GameObject in the game.
    Experimentation is done now. Flare is not designed to rip the game apart. Or maybe it is. At least not like this.

--- include/CustomTypes/particleController.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "custom-types/shared/macros.hpp"
-
-#include "UnityEngine/MonoBehaviour.hpp"
-
-DECLARE_CLASS_CODEGEN(Flare, ParticleController, UnityEngine::MonoBehaviour,
-    DECLARE_INSTANCE_METHOD(void, Update);
-)
