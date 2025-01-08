8c454a9f9ae60b7ac058830c8abfadfe06a14929
CatsaCode <catsacode@gmail.com>
2025-01-07 21:50:15 -0500

    Add text next to FloatSettings and MinMaxCurveSettings
    Added the setting's name next to the input instead of being the placeholder. New placeholder is 0.0.
    Showing the name to the left makes it always visible and quick to read, as opposed to before where you had to delete the current text or hover over it for the hover hint. The new placeholder text of 0.0 shows that you can write a decimal and if nothing is written then the value defaults to 0.

--- src/Window/createModuleWindows.cpp
+++ src/Window/createModuleWindows.cpp
@@ -26,7 +26,9 @@ namespace Flair::Window {
         verticalFitter->set_verticalFit(__ContentSizeFitter__FitMode::PreferredSize); // Use the height from child objects
 
         // Declare common object names that may be reused
-        HorizontalLayoutGroup* row;
+        GameObject* row;
+        RectTransform* rowTransform;
+        HorizontalLayoutGroup* rowLayout;
 
         // Create each setting
         CreateFloatSetting(vertical->get_transform(), "Duration", "The length of time the Particle System is emitting particles. If the system is looping, this indicates the length of one cycle.", ParticleController::MainModule::get_duration(particleSystem), [particleSystem](float value){ParticleController::MainModule::set_duration(particleSystem, value);});
@@ -37,16 +39,28 @@ namespace Flair::Window {
         CreateMinMaxCurveSetting(vertical->get_transform(), "Start speed", "The start speed of particles, applied in the starting direction.", ParticleController::MainModule::get_startSpeed(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startSpeed(particleSystem, value);});
         CreateBoolSetting(vertical->get_transform(), "3D Start size", "If enabled, you can control the size separately for each axis.", ParticleController::MainModule::get_startSize3D(particleSystem), [particleSystem](bool value){ParticleController::MainModule::set_startSize3D(particleSystem, value);});
         CreateMinMaxCurveSetting(vertical->get_transform(), "Start size", "The start size of particles.", ParticleController::MainModule::get_startSize(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startSize(particleSystem, value);});
-        row = BSML::Lite::CreateHorizontalLayoutGroup(vertical);
-        CreateMinMaxCurveSetting(row->get_transform(), "X", "", ParticleController::MainModule::get_startSizeX(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startSizeX(particleSystem, value);});
-        CreateMinMaxCurveSetting(row->get_transform(), "Y", "", ParticleController::MainModule::get_startSizeY(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startSizeY(particleSystem, value);});
-        CreateMinMaxCurveSetting(row->get_transform(), "Z", "", ParticleController::MainModule::get_startSizeZ(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startSizeZ(particleSystem, value);});
+        row = GameObject::New_ctor("3MinMaxCurveSetting");
+        row->get_transform()->SetParent(vertical->get_transform(), false);
+        rowTransform = row->AddComponent<RectTransform*>();
+        rowTransform->set_sizeDelta({0, 8});
+        rowLayout = row->AddComponent<HorizontalLayoutGroup*>();
+        rowLayout->set_childControlWidth(true);
+        rowLayout->set_childControlHeight(false);
+        CreateMinMaxCurveSetting(row->get_transform(), "", "", ParticleController::MainModule::get_startSizeX(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startSizeX(particleSystem, value);});
+        CreateMinMaxCurveSetting(row->get_transform(), "", "", ParticleController::MainModule::get_startSizeY(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startSizeY(particleSystem, value);});
+        CreateMinMaxCurveSetting(row->get_transform(), "", "", ParticleController::MainModule::get_startSizeZ(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startSizeZ(particleSystem, value);});
         CreateBoolSetting(vertical->get_transform(), "3D Start rotation", "If enabled, you can control the rotation separately for each axis.", ParticleController::MainModule::get_startRotation3D(particleSystem), [particleSystem](bool value){ParticleController::MainModule::set_startRotation3D(particleSystem, value);});
         CreateMinMaxCurveSetting(vertical->get_transform(), "Start rotation", "The start rotation of particles in degrees.", ParticleController::MainModule::get_startRotation(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startRotation(particleSystem, value);});
-        row = BSML::Lite::CreateHorizontalLayoutGroup(vertical);
-        CreateMinMaxCurveSetting(row->get_transform(), "X", "", ParticleController::MainModule::get_startRotationX(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startRotationX(particleSystem, value);});
-        CreateMinMaxCurveSetting(row->get_transform(), "Y", "", ParticleController::MainModule::get_startRotationY(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startRotationY(particleSystem, value);});
-        CreateMinMaxCurveSetting(row->get_transform(), "Z", "", ParticleController::MainModule::get_startRotationZ(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startRotationZ(particleSystem, value);});
+        row = GameObject::New_ctor("3MinMaxCurveSetting");
+        row->get_transform()->SetParent(vertical->get_transform(), false);
+        rowTransform = row->AddComponent<RectTransform*>();
+        rowTransform->set_sizeDelta({0, 8});
+        rowLayout = row->AddComponent<HorizontalLayoutGroup*>();
+        rowLayout->set_childControlWidth(true);
+        rowLayout->set_childControlHeight(false);
+        CreateMinMaxCurveSetting(row->get_transform(), "", "", ParticleController::MainModule::get_startRotationX(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startRotationX(particleSystem, value);});
+        CreateMinMaxCurveSetting(row->get_transform(), "", "", ParticleController::MainModule::get_startRotationY(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startRotationY(particleSystem, value);});
+        CreateMinMaxCurveSetting(row->get_transform(), "", "", ParticleController::MainModule::get_startRotationZ(particleSystem), [particleSystem](ParticleController::MinMaxCurve value){ParticleController::MainModule::set_startRotationZ(particleSystem, value);});
         CreateFloatSetting(vertical->get_transform(), "Flip rotation", "Cause some particles to spin in the opposite direction. (Set between 0 and 1, where a higher value causes more to flip)", ParticleController::MainModule::get_flipRotation(particleSystem), [particleSystem](float value){ParticleController::MainModule::set_flipRotation(particleSystem, value);});
         // TODO Crashes...?
         // CreateMinMaxGradientSetting(vertical->get_transform(), "Start color", "The start color of particles.", ParticleController::MainModule::get_startColor(particleSystem), [particleSystem](ParticleController::MinMaxGradient value){ParticleController::MainModule::set_startColor(particleSystem, value);});
--- src/Window/createSettings.cpp
+++ src/Window/createSettings.cpp
@@ -5,9 +5,14 @@
 
 #include "bsml/shared/BSML.hpp"
 
+#include "UnityEngine/Object.hpp"
+#include "UnityEngine/UI/LayoutElement.hpp"
+#include "UnityEngine/UI/ContentSizeFitter.hpp"
+
 #include <sstream>
 
 using namespace UnityEngine;
+using namespace UnityEngine::UI;
 
 namespace Flair::Window {
 
@@ -36,8 +41,37 @@ namespace Flair::Window {
             catch(...) {}; // TODO Make an actual numpad
         };
 
-        // Create object from BSML template
-        auto floatSetting = BSML::Lite::CreateStringSetting(parent, name, startValueSS.str(), onChangeString);
+        // Create container for the nameText and stringSetting
+        auto floatSetting = GameObject::New_ctor("FloatSetting");
+        floatSetting->get_transform()->SetParent(parent, false);
+        auto floatSettingTransform = floatSetting->AddComponent<RectTransform*>();
+        floatSettingTransform->set_sizeDelta({0, 8}); // Set specific height for the VerticalLayoutGroup
+
+        if(!name.empty()) {
+            // Create text from BSML template
+            auto nameText = BSML::Lite::CreateText(floatSetting->get_transform(), name, {0, 0}, {0, 0});
+            // Control layout with RectTransform
+            Object::DestroyImmediate(nameText->GetComponent<LayoutElement*>());
+            auto nameTextTransform = nameText->GetComponent<RectTransform*>();
+            nameTextTransform->set_anchorMin({0, 0});
+            nameTextTransform->set_anchorMax({1, 1});
+            nameTextTransform->set_pivot({0, 0.5});
+            nameTextTransform->set_anchoredPosition({0, 0});
+            nameTextTransform->set_sizeDelta({0, 0});
+            nameText->set_verticalAlignment(TMPro::VerticalAlignmentOptions::Middle);
+        }
+
+
+        // Create string setting from BSML template
+        auto stringSetting = BSML::Lite::CreateStringSetting(floatSetting->get_transform(), "0.0", startValueSS.str(), onChangeString);
+        Object::DestroyImmediate(stringSetting->GetComponent<LayoutElement*>()); // Don't let it override the width
+        auto stringSettingTransform = stringSetting->GetComponent<RectTransform*>();
+        stringSettingTransform->set_anchorMin({0.5, 0});
+        if(name.empty()) stringSettingTransform->set_anchorMin({0, 0}); // Take up full area if no text is given
+        stringSettingTransform->set_anchorMax({1, 1});
+        stringSettingTransform->set_pivot({1, 0.5});
+        stringSettingTransform->set_anchoredPosition({0, 0});
+        stringSettingTransform->set_sizeDelta({0, 0});
 
         // Add a hover hint for more information
         if(!hoverHint.empty()) BSML::Lite::AddHoverHint(floatSetting, hoverHint);
@@ -48,24 +82,15 @@ namespace Flair::Window {
     // Create a MinMaxCurve setting for a window
     GameObject* CreateMinMaxCurveSetting(Transform* parent, std::string name, std::string hoverHint, ParticleController::MinMaxCurve startValue, std::function<void(ParticleController::MinMaxCurve value)> onChange) {
         // TODO Create a MinMaxCurveWindow
-        
-        // Convert startValue to a string without the trailing zeros
-        std::stringstream startValueSS;
-        startValueSS << startValue.constantMax;
 
-        // Set up the function that converts the typed string into a MinMaxCurve for the provided onChange function
-        std::function<void(std::string)> onChangeString = [onChange](std::string value){
+        // Set up the function that converts the typed float into a MinMaxCurve for the provided onChange function
+        std::function<void(float)> onChangeFloat = [onChange](float value) {
             if(!onChange) return;
-            if(value.empty()) value = "0";
-            try {onChange(std::stof(value));}
-            catch(...) {}; // TODO Make an actual numpad
+            onChange(value);
         };
 
-        // Create object from BSML template
-        auto minMaxCurveSetting = BSML::Lite::CreateStringSetting(parent, name, startValueSS.str(), onChangeString);
-
-        // Add a hover hint for more information
-        if(!hoverHint.empty()) BSML::Lite::AddHoverHint(minMaxCurveSetting, hoverHint);
+        // Create temporary object from CreateFloatSetting
+        auto minMaxCurveSetting = CreateFloatSetting(parent, name, hoverHint, startValue.constantMax, onChangeFloat);
 
         return minMaxCurveSetting->get_gameObject();
     }
