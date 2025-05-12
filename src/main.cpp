420dc2c19eb2adb86633cc68e7035f81860367aa
CatsaCode <catsacode@gmail.com>
2025-05-11 22:45:06 -0400

    Load and log cube vertices
    Wrote a small test in main.cpp to load testCube.glb and print the vertex positions to the terminal.
    Test to see if assimp importing works.

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@
 #include "Window/window.hpp"
 #include "Window/createModuleWindows.hpp"
 
+#include "assimp/shared/assimp/Importer.hpp"
+#include "assimp/shared/assimp/scene.h"
+#include "assimp/shared/assimp/postprocess.h"
+
 
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 // Stores the ID and version of our mod, and is sent to
@@ -29,21 +33,47 @@ Configuration &getConfig() {
 MAKE_HOOK_MATCH(TestHook, &GlobalNamespace::MainMenuViewController::DidActivate, void, GlobalNamespace::MainMenuViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
     TestHook(self, firstActivation, addedToHierarchy, screenSystemEnabling);
 
-    // if(!firstActivation) return;
+    PaperLogger.info("Loading file...");
 
-    static UnityEngine::ParticleSystem* dustParticleSystem = nullptr;
+    std::string file = "/storage/emulated/0/ModData/com.beatgames.beatsaber/Mods/Flair/testCube.glb";
 
-    if(dustParticleSystem == nullptr) for(auto particle : UnityEngine::Resources::FindObjectsOfTypeAll<UnityEngine::ParticleSystem*>()) if(particle->name == "DustPS") {
-        dustParticleSystem = particle;
-        break;
-    }
-    if(dustParticleSystem == nullptr) {
-        PaperLogger.error("Could not find DustPS");
+    Assimp::Importer importer;
+
+    const aiScene* scene = importer.ReadFile(file, 0);
+
+    if(scene == nullptr) {
+        PaperLogger.info("Error!");
+        PaperLogger.error("{}", importer.GetErrorString());
         return;
     }
 
-    if(rand() % 2 == 0) Flair::Window::CreateWindow(nullptr, "Test Window", {40, 100});
-    else Flair::Window::CreateMainModuleWindow(nullptr, dustParticleSystem);
+    for(int i = 0; i < scene->mNumMeshes; i++) {
+        aiMesh* mesh = scene->mMeshes[i];
+        PaperLogger.info("Mesh name: {}", mesh->mName.C_Str());
+
+        for(int j = 0; j < mesh->mNumVertices; j++) {
+            aiVector3D vertex = mesh->mVertices[j];
+            PaperLogger.info("X: {}, Y: {}, Z: {}", vertex.x, vertex.y, vertex.z);
+        }
+    }
+
+
+
+    // // if(!firstActivation) return;
+
+    // static UnityEngine::ParticleSystem* dustParticleSystem = nullptr;
+
+    // if(dustParticleSystem == nullptr) for(auto particle : UnityEngine::Resources::FindObjectsOfTypeAll<UnityEngine::ParticleSystem*>()) if(particle->name == "DustPS") {
+    //     dustParticleSystem = particle;
+    //     break;
+    // }
+    // if(dustParticleSystem == nullptr) {
+    //     PaperLogger.error("Could not find DustPS");
+    //     return;
+    // }
+
+    // if(rand() % 2 == 0) Flair::Window::CreateWindow(nullptr, "Test Window", {40, 100});
+    // else Flair::Window::CreateMainModuleWindow(nullptr, dustParticleSystem);
 }
 
 
