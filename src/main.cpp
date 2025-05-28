3217df2034cc268d2530061f080ba962332e0e0b
CatsaCode <catsacode@gmail.com>
2025-05-27 22:15:10 -0400

    Create Unity mesh and instantiate
    Create a UnityEngine::Mesh object and convert the assimp vertex and face data into vertices and triangles. Instantiate this mesh in a visible way.
    Assimp data is meaningless to Unity unless it is converted.

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,14 @@
 #include "assimp/shared/assimp/Importer.hpp"
 #include "assimp/shared/assimp/scene.h"
 #include "assimp/shared/assimp/postprocess.h"
+#include "assimp/shared/assimp/mesh.h"
+#include "UnityEngine/Mesh.hpp"
+#include "UnityEngine/MeshRenderer.hpp"
+#include "UnityEngine/MeshFilter.hpp"
+#include "UnityEngine/MeshTopology.hpp"
+#include "UnityEngine/Rendering/IndexFormat.hpp"
+
+#include "bsml/shared/Helpers/getters.hpp"
 
 
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
@@ -30,16 +38,18 @@ Configuration &getConfig() {
 }
 
 
-MAKE_HOOK_MATCH(TestHook, &GlobalNamespace::MainMenuViewController::DidActivate, void, GlobalNamespace::MainMenuViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
-    TestHook(self, firstActivation, addedToHierarchy, screenSystemEnabling);
+MAKE_HOOK_MATCH(AssimpTestHook, &GlobalNamespace::MainMenuViewController::DidActivate, void, GlobalNamespace::MainMenuViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
+    AssimpTestHook(self, firstActivation, addedToHierarchy, screenSystemEnabling);
 
     PaperLogger.info("Loading file...");
 
     std::string file = "/storage/emulated/0/ModData/com.beatgames.beatsaber/Mods/Flair/testCube.glb";
+    // std::string file = "/storage/emulated/0/ModData/com.beatgames.beatsaber/Mods/Flair/teapot.glb";
 
     Assimp::Importer importer;
 
-    const aiScene* scene = importer.ReadFile(file, 0);
+    const aiScene* scene = importer.ReadFile(file, 
+        aiProcess_Triangulate);
 
     if(scene == nullptr) {
         PaperLogger.info("Error!");
@@ -50,11 +60,42 @@ MAKE_HOOK_MATCH(TestHook, &GlobalNamespace::MainMenuViewController::DidActivate,
     for(int i = 0; i < scene->mNumMeshes; i++) {
         aiMesh* mesh = scene->mMeshes[i];
         PaperLogger.info("Mesh name: {}", mesh->mName.C_Str());
+        
+        UnityEngine::Mesh* unityMesh = UnityEngine::Mesh::New_ctor();
+        unityMesh->set_name(mesh->mName.C_Str());
+        unityMesh->Clear();
 
+        ArrayW<UnityEngine::Vector3> vertices = ArrayW<UnityEngine::Vector3>(mesh->mNumVertices);
         for(int j = 0; j < mesh->mNumVertices; j++) {
             aiVector3D vertex = mesh->mVertices[j];
-            PaperLogger.info("X: {}, Y: {}, Z: {}", vertex.x, vertex.y, vertex.z);
+            PaperLogger.info("Vertex {} | X: {}, Y: {}, Z: {}", j, vertex.x, vertex.y, vertex.z);
+            vertices[j] = UnityEngine::Vector3(vertex.x, vertex.y, vertex.z);
         }
+        unityMesh->set_vertices(vertices);
+
+        ArrayW<int> triangles = ArrayW<int>(mesh->mNumFaces * 3);
+        for(int j = 0; j < mesh->mNumFaces; j++) {
+            aiFace face = mesh->mFaces[j];
+            for(int k = 0; k < 3; k++) {
+                PaperLogger.info("Face {} Index {}", j, face.mIndices[k]);
+                triangles[j * 3 + k] = face.mIndices[k];
+            }
+        }
+        unityMesh->set_triangles(triangles);
+
+
+
+        UnityEngine::GameObject* testGO = UnityEngine::GameObject::New_ctor("FlairTest");
+        testGO->get_transform()->set_position(UnityEngine::Vector3(0, 0.5, 0.5));
+        testGO->get_transform()->set_localScale(UnityEngine::Vector3(0.2, 0.2, 0.2));
+
+        UnityEngine::MeshFilter* testFilter = testGO->AddComponent<UnityEngine::MeshFilter*>();
+        testFilter->set_sharedMesh(unityMesh);
+        
+        UnityEngine::MeshRenderer* testRenderer = testGO->AddComponent<UnityEngine::MeshRenderer*>();
+        UnityEngine::Shader* shader = UnityEngine::Resources::FindObjectsOfTypeAll<UnityEngine::Shader*>()->FirstOrDefault([](UnityEngine::Shader* shader){return shader->get_name() == "Standard";});
+        UnityEngine::Material* material = UnityEngine::Material::New_ctor(shader);
+        testRenderer->set_sharedMaterial(material);
     }
 
 
@@ -75,6 +116,7 @@ MAKE_HOOK_MATCH(TestHook, &GlobalNamespace::MainMenuViewController::DidActivate,
     // if(rand() % 2 == 0) Flair::Window::CreateWindow(nullptr, "Test Window", {40, 100});
     // else Flair::Window::CreateMainModuleWindow(nullptr, dustParticleSystem);
 }
+
 
 
 // Called at the early stages of game loading
@@ -101,7 +143,7 @@ MOD_EXTERN_FUNC void late_load() noexcept {
 
     PaperLogger.info("Installing hooks...");
 
-    INSTALL_HOOK(PaperLogger, TestHook);
+    INSTALL_HOOK(PaperLogger, AssimpTestHook);
 
     PaperLogger.info("Installed all hooks!");
 }
